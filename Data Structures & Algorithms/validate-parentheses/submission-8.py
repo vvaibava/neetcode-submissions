732ec34037be9813b27f21b0823d3424f0062716
vvaibava <65467493+vvaibava@users.noreply.github.com>
2026-06-16 23:52:44 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1: 
+            return False
+
+        pars = {"[":"]", "{":"}", "(":")"}
+        stack = []
+        for i in range(len(s)): 
+            if s[i] not in pars: 
+                if stack and s[i] == pars[stack[-1]]:
+                    stack.pop()
+                    continue
+                else:
+                    return False
+            stack.append(s[i])
+        return len(stack) == 0
